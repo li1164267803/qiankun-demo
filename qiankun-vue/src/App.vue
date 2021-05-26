8faa8c18f631c6d342ba9ebbe816d5cf90782ad4
xiwen <lilin0@aixuexi.com>
2021-05-26 13:39:32 +0800

    code

--- qiankun-vue/src/App.vue
+++ qiankun-vue/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">vue子页面的内容</div>
+    <p>子应用的样式不会影响 别的微应用 </p>
+    <p>基于 shadow DOM 的样式隔离</p>
     <router-view />
   </div>
 </template>
@@ -13,3 +15,10 @@ export default {
   },
 }
 </script>
+
+<style  scoped>
+div,a {
+  color: red;
+  font-size: 32px;
+}
+</style>
